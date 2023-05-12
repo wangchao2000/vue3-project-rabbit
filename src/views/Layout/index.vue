2653930b4654247a259576ae16d0c541fb12fd82
Alan <2283081725@qq.com>
2023-05-12 10:29:16 +0800

    scss文件的自动导入

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,15 +1,25 @@
 <template>
+  <div>
+    首页
     <div>
-        首页
-        <!-- 二级路由出口 -->
-        <RouterView/>
+      <button @click="increment" class="test">{{ count }}</button>--{{ doubleCount }}
     </div>
+    <!-- 二级路由出口 -->
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
+import { useCounterStore } from "@/stores/counter";
+import { storeToRefs } from "pinia";
 
+
+const { count, doubleCount } = storeToRefs(useCounterStore());
+const { increment } = useCounterStore();
 </script>
 
 <style lang="scss" scoped>
-
+.test{
+    color: $priceColor;
+}
 </style>
