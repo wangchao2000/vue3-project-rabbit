376bc7513d998d6be7487e8ce72118a9e257e6de
Alan <2283081725@qq.com>
2023-05-17 10:27:12 +0800

    轮播图的实现

---
+++ src/views/Home/components/HomeNew.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        HomeNew页面
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Home/components/HomeProduct.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        HomeProduct页面
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
