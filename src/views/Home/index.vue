3f95b14857831f626bbab7acb76d07f92680874c
Alan <2283081725@qq.com>
2023-05-17 11:01:59 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,22 +1,28 @@
 <script setup>
-import HomeBanner from './components/HomeBanner.vue';
-import HomeCategory from './components/HomeCategory.vue';
-import HomeHot from './components/HomeHot.vue';
-import HomeNew from './components/HomeNew.vue';
-import HomeProduct from './components/HomeProduct.vue';
+import HomeBanner from "./components/HomeBanner.vue";
+import HomeCategory from "./components/HomeCategory.vue";
+import HomeHot from "./components/HomeHot.vue";
+import HomeNew from "./components/HomeNew.vue";
+import HomeProduct from "./components/HomeProduct.vue";
+import HomePanel from "./components/HomePanel.vue";
 </script>
 
 <template>
-    <div class="container">
-       <HomeCategory/>
-       <HomeBanner/>
-    </div>
-    <HomeNew/>
-    <HomeHot/>
-    <HomeProduct/>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>我是新鲜好物的插槽内容</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>我是人气推荐的插槽内容</div>
+  </HomePanel>
 </template>
 
 
 <style lang="scss" scoped>
-
 </style>
